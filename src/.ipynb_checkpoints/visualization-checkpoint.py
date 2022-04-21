ad8391adb6601e48ae231902b189da84cb83ab15
MeirYosub <meir.yosub@gmail.com>
2022-04-21 15:05:15 +0300

    more fixes to visualization

--- src/.ipynb_checkpoints/visualization-checkpoint.py
+++ src/.ipynb_checkpoints/visualization-checkpoint.py
@@ -1,7 +1,7 @@
 import plotly.graph_objects as go
 import numpy as np
 
-def make_meshgrid(x, y, h=.01):
+def make_meshgrid(x, y, h=.02):
     x_min, x_max = x.min() - 1, x.max() + 1
     y_min, y_max = y.min() - 1, y.max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
@@ -12,7 +12,13 @@ def make_decision_boundary_plot(clf_fit,X0,X1,y,x_grid,y_grid,title):
     Z = clf_fit.predict(np.c_[x_grid, y_grid])
     fig = go.Figure(data = go.Contour(x = x_grid, y = y_grid, 
                     z = Z,colorscale=[[0, 'rgb(204,229,255)'],[1,'rgb(255,153,150)']],showscale=False))
-    fig=fig.add_trace(go.Scatter(x=X0,y=X1,
-                    mode = 'markers',marker=dict(color=y,size=8,line=dict(color='black',width=1))))
-    fig.update_layout(title=title,height=800)
+    
+    
+    fig=fig.add_trace(go.Scatter(name='Class 0',x=X0[y==0],y=X1[y==0],
+                    mode = 'markers',marker=dict(color='Blue',size=8,line=dict(color='black',width=1))))
+    fig=fig.add_trace(go.Scatter(name='Class 1',x=X0[y==1],y=X1[y==1],
+                    mode = 'markers',marker=dict(color='Red',size=8,line=dict(color='black',width=1))))
+    
+    fig.update_layout(legend=dict(yanchor="top",y=0.99,xanchor="left",x=0.01))
+    fig.update_layout(title=title,width=1000,height=800)
     return fig
